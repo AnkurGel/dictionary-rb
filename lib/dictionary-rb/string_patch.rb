44b990d8b9f93c68ad6f0f292d3b1c33b4382253
Ankur Goel <ankurgel@gmail.com>
2014-02-01 03:45:18 +0530

    Patched string class for additional methods. MetaProgramming++

--- lib/dictionary-rb/string_patch.rb
+++ lib/dictionary-rb/string_patch.rb
@@ -1,13 +1,21 @@
 class String
-  def meaning
-    DictionaryRB::Word.new(self)
+  self.class_eval do
+    %w(meaning urban_meaning to_urban to_dictionary).each do |method|
+      define_method method do
+        if obj=method.match(/^to_(.*)/)
+          DictionaryRB::Word.new(self).method(obj[1]).call
+        else
+          DictionaryRB::Word.new(self).method(method).call
+        end
+      end
+    end
+
+    def to_word
+      DictionaryRB::Word.new(self)
+    end
   end
 
-  def urban_meaning
-    DictionaryRB::Word.new(self).urban_meaning
-  end
-
-  def to_urban
-    DictionaryRB::Word.new(self).urban
+  def respond_to?(method)
+    %w(meaning urban_meaning to_urban to_dictionary to_word).include?(method) || super
   end
 end
